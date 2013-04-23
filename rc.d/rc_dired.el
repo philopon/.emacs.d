8421ceb2593349e3358d34e2311727a3c7872d79
philopon <philopon.dependence@gmail.com>
2013-04-24 02:53:16 +0900

    refine rc_popwin

---
+++ rc.d/rc_dired.el
@@ -0,0 +1,18 @@
+;; http://qiita.com/items/13585a5711d62e9800ef
+(defun dired-my-append-buffer-name-hint ()
+  "Append a auxiliary string to a name of dired buffer."
+  (when (eq major-mode 'dired-mode)
+      (rename-buffer (concat "[Dired] " (buffer-name)) t)))
+
+(add-hook 'dired-mode-hook 'dired-my-append-buffer-name-hint)
+
+(require 'wdired)
+(define-key dired-mode-map "r" 'wdired-change-to-wdired-mode)
+
+(require 'dired-x)
+(require 'rc_popwin)
+
+(push '(dired-mode :position left :width 100) popwin:special-display-config)
+(global-set-key (kbd "C-x d") 'dired-jump-other-window)
+
+(provide 'rc_dired)
